adfe5ed96616aa45f2aa9f713734c74e853ceca3
Dan <dan@github.com>
2018-10-07 12:46:22 +0100

    Add POST /todos route and refactor moongoose

---
+++ server/server.js
@@ -0,0 +1,30 @@
+//libary imports
+var express = require('express');
+var bodyParser = require('body-parser');
+
+//local imports
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+//this url is for resource creation to create todo's
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text: req.body.text
+    });
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.send(e);
+    });
+});
+
+// GET/ todos  method to read the todos
+
+app.listen(3000, () => {
+    console.log('Started on port 3000');
+});
